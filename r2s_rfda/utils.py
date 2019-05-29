de4635f5103074b12010c3fbf147743fe383ad1a
Roman Rodionov <rodionov.rn@yandex.ru>
2019-05-29 13:41:11 +0300

    Add tests for settings save/load

--- r2s_rfda/utils.py
+++ r2s_rfda/utils.py
@@ -1,10 +1,4 @@
 import pickle
-from pathlib import Path
-
-from .constants import TIME_UNITS
-
-
-__all__ = ['adjust_time']
 
 
 def save_config(cwd, **conf_params):
@@ -40,44 +34,3 @@ def load_config(cwd):
         conf_params = pickle.load(f)
     return conf_params
 
-
-def fetch_folder(folder, read_only=False):
-    """Fetches the specified folder.
-
-    If the specified folder does not exist then it will be
-    created. If the file with a such name already exist, then
-    exception will be raised. read_only flag can be used to
-    not overwrite already existing folder.
-
-    Parameters
-    ----------
-    folder : str
-        Folder name.
-    read_only : bool
-        Flag if the folder is readonly - not to overwrite the results.
-
-    Returns
-    -------
-    path : Path
-        Path object of the folder.
-    """
-    path = Path(folder)
-    if path.exists() and not path.is_dir():
-        raise FileExistsError("Such file exists but it is not a folder.")
-    elif not path.exists():
-        if read_only:
-            raise FileNotFoundError("Data directory not found")
-        else:
-            path.mkdir()
-    return path
-
-
-_sort_units = ('YEARS', 'DAYS', 'HOURS', 'MINS', 'SECS')
-
-
-def adjust_time(time):
-        for unit in _sort_units:
-            d = time / TIME_UNITS[unit]
-            if d >= 1:
-                return d, unit
-        return time, 'SECS'
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,25 +1,28 @@
+# -*- coding: utf-8 -*-
+
 import pytest
+from pathlib import Path
+import shutil
 
-from r2s_rfda.utils import *
+from r2s_rfda import utils
 
 
-@pytest.mark.parametrize('time, value, unit', [
-    (0.5, 0.5, 'SECS'),
-    (12, 12, 'SECS'),
-    (59, 59, 'SECS'),
-    (75, 1.25, 'MINS'),
-    (120, 2.0, 'MINS'),
-    (3540, 59, 'MINS'),
-    (3600, 1.0, 'HOURS'),
-    (7200, 2.0, 'HOURS'),
-    (23*3600, 23.0, 'HOURS'),
-    (24*3600, 1.0, 'DAYS'),
-    (36*3600, 1.5, 'DAYS'),
-    (363*24*3600, 363.0, 'DAYS'),
-    (365*24*3600, 1.0, 'YEARS'),
-    (2*365*24*3600, 2.0, 'YEARS')
+root = Path(__file__).resolve().parent
+
+
+@pytest.mark.parametrize('folder, config', [
+    ('test1', {
+        'approach': 'full', 'task_list': [('folder1', ['task1', 'task2']), 
+        ('folder2', ['task1', 'task2', 'task3'])], 'cells': [1, 3, 4, 5, 8],
+        'materials': [2, 100, 400]
+    })
 ])
-def test_adjust_time(time, value, unit):
-    adj_val, adj_unit = adjust_time(time)
-    assert adj_unit == unit
-    assert adj_val == value
+def test_config(folder, config):
+    path = root / folder
+    path.mkdir()
+
+    utils.save_config(path, **config)
+
+    load_conf = utils.load_config(path)
+    assert load_conf == config
+    shutil.rmtree(path)
