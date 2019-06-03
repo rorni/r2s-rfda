e659253ea45264d2f5348c87739812b5977047db
Roman Rodionov <rodionov.rn@yandex.ru>
2019-06-03 14:48:46 +0300

    Add fispact output reading function header

--- r2s_rfda/fetch.py
+++ r2s_rfda/fetch.py
@@ -30,4 +30,28 @@ def load_gamma(path):
         Gamma yield distribution.
     """
     raise NotImplementedError
-    
+
+
+def read_fispact_output(path):
+    """Reads FISPACT output file.
+
+    Parameters
+    ----------
+    path : Path
+        Path to output file.
+
+    Returns
+    -------
+    time_labels : list
+        Labels of time moments.
+    ebins : list
+        list of gamma energy bin boundaries.
+    atoms : dict
+        A dictionary of the number of atoms. (time_label, isotope) -> atoms.
+    activity : dict
+        A dictionary of the isotope activities. (time_label, isotope) -> activity.
+    gamma_yield : dict
+        A dictionary of the decay gamma intensity. (time_label, en_label) -> gamma yield 
+        [gamma/sec]
+    """
+    pass
