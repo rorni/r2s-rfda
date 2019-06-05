bc06dc253b59ee9c8522c06b140a0fc0fe81c04d
Roman Rodionov <rodionov.rn@yandex.ru>
2019-06-06 00:40:06 +0300

    Add tests for bin distribution creations

--- test/test_source.py
+++ test/test_source.py
@@ -5,4 +5,15 @@ import pytest
 from r2s_rfda import source
 
 
+@pytest.mark.parametrize('bins, start_name', [
+    ([1, 2, 3, 4], 1), ([1, 2, 3, 4], 2)
+])
+def test_create_bin_distributions(bins, start_name):
+    new_name, distrs = source.create_bin_distributions(bins, start_name)
+    assert len(distrs) == len(bins) - 1
+    assert new_name == start_name + len(distrs)
+    for i, d in enumerate(distrs):
+        assert d._values == [bins[i], bins[i+1]]
+        assert d._probs == [1]
+        assert d.name == start_name + i
 
