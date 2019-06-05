ad284cc78378b951d057e2e41a643ce3d58199bd
davlet <kumpilov95@mail.ru>
2019-06-05 15:18:27 +0300

    Implement fetch.collect func

--- r2s_rfda/fetch.py
+++ r2s_rfda/fetch.py
@@ -2,12 +2,13 @@
 
 import pypact as pp
 import numpy as np
+import pickle
 
 from itertools import accumulate
 
 
 def collect(path, config):
-    """Collects data from files.
+    """Collects all data from inventory files and writes total files on disk.
 
     Parameters
     ----------
@@ -16,7 +17,24 @@ def collect(path, config):
     config : dict
         Dictionary of configuration data.
     """
-    raise NotImplementedError
+    A = {}
+    N = {}
+    G = {}
+    for index, casepath in config['index_output'].items():
+        time_labels, ebins, atoms, activity, gamma_yield = read_fispact_output(casepath)
+        for (t, nuc), act in activity.items():
+            A[(t, nuc, *index)] = act
+        for (t, nuc), number in atoms.items():
+            N[(t, nuc, *index)] = number
+        for t, gamma_ar in gamma_yield.items():
+            for i, gam in enumerate(gamma_ar):
+                G[(t, i, *index)] = gam
+    with open(path / 'activity.dat', 'bw') as f:
+        pickle.dump(A, f, pickle.HIGHEST_PROTOCOL)
+    with open(path / 'atoms.dat', 'bw') as f:
+        pickle.dump(N, f, pickle.HIGHEST_PROTOCOL)
+    with open(path / 'gamma.dat', 'bw') as f:
+        pickle.dump(G, f, pickle.HIGHEST_PROTOCOL)
 
 
 def load_gamma(path):
@@ -50,9 +68,9 @@ def read_fispact_output(path):
     ebins : list
         list of gamma energy bin boundaries.
     atoms : dict
-        A dictionary of the number of atoms. (time_label, isotope) -> atoms.
+        A dictionary of the number of atoms. (time_label, nuclide) -> atoms.
     activity : dict
-        A dictionary of the isotope activities. (time_label, isotope) -> activity.
+        A dictionary of the nuclide activities. (time_label, nuclide) -> activity.
     gamma_yield : dict
         A dictionary of the decay gamma intensity. time_label ->
         gamma yield group spectrum [gamma/sec]
@@ -71,7 +89,7 @@ def read_fispact_output(path):
         time_labels.append(np.array(durations).sum())
         gamma_yield[time_labels[-1]] = np.array(ts.gamma_spectrum.values) / eners
         for nuc in ts.nuclides:
-            name = nuc.element + str(nuc.isotope)
+            name = nuc.element + str(nuc.isotope) + nuc.state
             atoms[(time_labels[-1], name)] = nuc.atoms
             activity[(time_labels[-1], name)] = nuc.activity
     return time_labels, ebins, atoms, activity, gamma_yield
