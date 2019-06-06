689eb3747a4c456a6adf803376ff8c58d1d03c25
Roman Rodionov <rodionov.rn@yandex.ru>
2019-06-06 11:46:16 +0300

    Add tests for source generation

--- test/test_source.py
+++ test/test_source.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+import sparse
 
 from r2s_rfda import source
+from r2s_rfda import data
 
 
 @pytest.mark.parametrize('bins, start_name', [
@@ -17,3 +19,127 @@ def test_create_bin_distributions(bins, start_name):
         assert d._probs == [1]
         assert d.name == start_name + i
 
+
+@pytest.mark.parametrize('gamma_data, timelab, start_distr, answer', [
+    (data.SparseData(
+        ('time', 'g_erg', 'xbins', 'ybins', 'zbins', 'cell'),
+        ((3, 10, 15, 20), (0, 1, 20), (-1, 1, 3), (-1, 2, 4), (2, 5), (200, 300)),
+        sparse.COO(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1],
+                [0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+                [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1]
+            ],
+            [10, 20, 15, 10, 15, 15, 20, 5, 10, 7.5, 5, 7.5, 7.5, 10, 2.5, 5, 3, 2.5, 3, 3, 5],
+            shape=(4, 2, 2, 2, 1, 2)
+        )
+     ), 3, 1,
+"""C total gamma intensity = 1.05000e+02
+SDEF PAR=2 EFF=0.01 CEL=D1 ERG=FCEL D2 X=FCEL D3 Y=FCEL D4 Z=FCEL D5
+SI1 L 300 300 200 300 200 300 200
+SP1 D 10.0 15.0 10.0 15.0 20.0 20.0 15.0
+DS2 S 6 6 7 7 7 7 7
+DS3 S 8 9 8 8 8 9 9
+DS4 S 10 10 10 10 11 10 11
+DS5 S 12 12 12 12 12 12 12
+SI6 H 0 1
+SP6 D 0 1
+SI7 H 1 20
+SP7 D 0 1
+SI8 H -1 1
+SP8 D 0 1
+SI9 H 1 3
+SP9 D 0 1
+SI10 H -1 2
+SP10 D 0 1
+SI11 H 2 4
+SP11 D 0 1
+SI12 H 2 5
+SP12 D 0 1"""
+    ),
+    (data.SparseData(
+        ('g_erg', 'xbins', 'ybins', 'time', 'zbins', 'cell'),
+        ((0, 1, 20), (-1, 1, 3), (-1, 2, 4), (3, 10, 15, 20), (2, 5), (200, 300)),
+        sparse.COO(
+            [
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1],
+                [0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+                [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1]
+            ],
+            [10, 20, 15, 10, 15, 15, 20, 5, 10, 7.5, 5, 7.5, 7.5, 10, 2.5, 5, 3, 2.5, 3, 3, 5],
+            shape=(2, 2, 2, 4, 1, 2)
+        )
+     ), 3, 1,
+"""C total gamma intensity = 1.05000e+02
+SDEF PAR=2 EFF=0.01 CEL=D1 ERG=FCEL D2 X=FCEL D3 Y=FCEL D4 Z=FCEL D5
+SI1 L 300 300 200 300 200 300 200
+SP1 D 10.0 15.0 10.0 15.0 20.0 20.0 15.0
+DS2 S 6 6 7 7 7 7 7
+DS3 S 8 9 8 8 8 9 9
+DS4 S 10 10 10 10 11 10 11
+DS5 S 12 12 12 12 12 12 12
+SI6 H 0 1
+SP6 D 0 1
+SI7 H 1 20
+SP7 D 0 1
+SI8 H -1 1
+SP8 D 0 1
+SI9 H 1 3
+SP9 D 0 1
+SI10 H -1 2
+SP10 D 0 1
+SI11 H 2 4
+SP11 D 0 1
+SI12 H 2 5
+SP12 D 0 1"""
+    ),
+    (data.SparseData(
+        ('time', 'g_erg', 'xbins', 'ybins', 'zbins', 'cell'),
+        ((3, 10, 15, 20), (0, 1, 20), (-1, 1, 3), (-1, 2, 4), (2, 5), (200, 300)),
+        sparse.COO(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2],
+                [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1],
+                [0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+                [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1]
+            ],
+            [10, 20, 15, 10, 15, 15, 20, 5, 10, 7.5, 5, 7.5, 7.5, 10, 2.5, 5, 3, 2.5, 3, 3, 5],
+            shape=(4, 2, 2, 2, 1, 2)
+        )
+     ), 15, 1,
+"""C total gamma intensity = 2.40000e+01
+SDEF PAR=2 EFF=0.01 CEL=D1 ERG=FCEL D2 X=FCEL D3 Y=FCEL D4 Z=FCEL D5
+SI1 L 300 300 200 300 200 300 200
+SP1 D 2.5 3.0 2.5 3.0 5.0 5.0 3.0
+DS2 S 6 6 7 7 7 7 7
+DS3 S 8 9 8 8 8 9 9
+DS4 S 10 10 10 10 11 10 11
+DS5 S 12 12 12 12 12 12 12
+SI6 H 0 1
+SP6 D 0 1
+SI7 H 1 20
+SP7 D 0 1
+SI8 H -1 1
+SP8 D 0 1
+SI9 H 1 3
+SP9 D 0 1
+SI10 H -1 2
+SP10 D 0 1
+SI11 H 2 4
+SP11 D 0 1
+SI12 H 2 5
+SP12 D 0 1"""
+    )   
+])
+def test_create_source(gamma_data, timelab, start_distr, answer):
+    sdef = source.create_source(gamma_data, timelab, start_distr=start_distr)
+    print(sdef)
+    assert sdef == answer
