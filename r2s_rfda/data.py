59550dd01a7081dd7626adffe4b2a86cee3e6a15
Roman Rodionov <rodionov.rn@yandex.ru>
2019-06-19 23:10:00 +0300

    Add SparseIndex class

--- r2s_rfda/data.py
+++ r2s_rfda/data.py
@@ -1,188 +1,77 @@
 # -*- coding: utf-8 -*-
 
-import sparse
 import numpy as np
+from collections import defaultdict
 
 
-class SparseData:
-    """Represents multidimensional sparse data.
+class SpatialIndex:
+    """Spatial index of mesh geometry.
 
     Parameters
     ----------
-    axes : list[str]
-        A list of axis labels.
-    bins : dict[list]
-        A dictionary of axis bins. key - is axis label. Value - bin boundaries
-        or labels along the axis.
-    shape : tuple
-        Data shape. It is needed to judge if axis has been boundaries or labels.
-    data : sparse.COO or dict
-        Data. If it is already an array, then it is just copied. If it is a dict
-        it is used to construct sparse data array.
+    indices : iterable
+        A list of index tuples: (c, i, j, k).
 
     Methods
     -------
-    tensor_dot(data)
-        Tensor dot product with other SparseData object.
-    multiply(data)
-        Element-wise multiplication of the two arrays.
-    replace_axes() 
-        Copies the SparseData object and replaces specific axes.
+    label(index)
+        Gets labels that correspond to the index.
+    indices(c, i, j, k)
+        Gets indices that correspond to the labels.
     """
-    def __init__(self, axes, labels, data):
-        if len(axes) != len(labels):
-            raise ValueError('Inconsistent axes and labels')
-        self._axes = tuple(axes)
-        self._labels = tuple(tuple(x) for x in labels)
-        if isinstance(data, dict):
-            enum_rules = create_enum_rules(labels)
-            coords = np.empty((len(axes), len(data.items())))
-            spdata = np.empty(len(data.items()))
-            for i, (k, v) in enumerate(data.items()):
-                coords[:, i] = create_index(k, enum_rules)
-                spdata[i] = v
-            shape = tuple(len(l) for l in labels)
-            self._data = sparse.COO(coords, data=spdata, shape=shape)
-        elif isinstance(data, sparse.COO):
-            self._data = data
-        else:
-            raise TypeError('Unknown data type. data must be either dict or SparseData instance')
+    def __init__(self, indices):
+        self._c_labels = defaultdict(set)
+        self._i_labels = defaultdict(set)
+        self._j_labels = defaultdict(set)
+        self._k_labels = defaultdict(set)
+        self._labels = tuple(x for x in sorted(indices))
+        for q, (c, i, j, k) in enumerate(self._labels):
+            self._c_labels[c].add(q)
+            self._i_labels[i].add(q)
+            self._j_labels[j].add(q)
+            self._k_labels[k].add(q)       
 
-    def tensor_dot(self, data):
-        """Tensor dot product with other SparseData object.
-
-        It takes into account axis labels of both SparseData objects and shapes.
-        It finds coincident labels and do tensor dot product along those axes.
-        If either there is no coincident axis labels or dimensions of such axes
-        are different, exception TypeError is raised.
+    def label(self, index):
+        """Gets labels, that correspond to the index.
 
         Parameters
         ----------
-        data : SparseData
-            Another SparseData instance.
-
+        index : int
+            Index.
+        
         Returns
         -------
-        result : SparseData
-            Tensor dot product.
+        c : int 
+            Cell name.
+        i, j, k : int
+            Voxel labels.
         """
-        convolution_labels = set(self.axes).intersection(data.axes)
-        si = self.find_indices(convolution_labels)
-        di = data.find_indices(convolution_labels)
-        conv_data = sparse.tensordot(self.data, data.data, axes=(si, di))
-        conv_axes = []
-        conv_labels = []
-        for i, lab in enumerate(self.axes):
-            if i not in si:
-                conv_axes.append(lab)
-                conv_labels.append(self.labels[i])
-        for i, lab in enumerate(data.axes):
-            if i not in di:
-                conv_axes.append(lab)
-                conv_labels.append(data.labels[i])
-        return SparseData(conv_axes, conv_labels, conv_data)
+        return self._labels[index]
 
-    def find_indices(self, labels):
-        labels = labels.copy()
-        indices = []
-        for i, ax in enumerate(self._axes):
-            if ax in labels:
-                indices.append(i)
-                labels.remove(ax)
-        return tuple(indices)
-
-    def multiply(self, data):
-        """Do element-wise multiplication of two tensors.
-
-        Dimensions are not necessary to be coincident, but one of the tensors
-        must be a subset of another. In the later case the subset tensor will
-        be extended.
+    def indices(self, c=None, i=None, j=None, k=None):
+        """Gets indices that correspond to the labels.
 
         Parameters
         ----------
-        data : SparseData
-            Another SparseData instance.
+        c : int
+            Cell label. Default: None.
+        i, j, k : int
+            Voxel labels. Default: None.
 
         Returns
         -------
-        result : SparseData
-            Element-wise multiplication of two tensors.
+        index : list
+            Indices, that corresponds to the specified labels.
         """
-        sset = set(self.axes)
-        dset = set(data.axes)
-        if sset < dset:
-            return data.multiply(self)
-        elif sset >= dset:
-            diff = sset.difference(dset)
-            s_ind = enumerate_labels(self.axes)
-            dshape = [len(l) for l in data.labels]
-            daxes = list(data.axes)
-            dlabels = list(data.labels)
-            for dl in diff:
-                dshape.insert(0, len(self.labels[s_ind[dl]]))
-                daxes.insert(0, dl)
-                dlabels.insert(0, self.labels[s_ind[dl]])
-            if len(dshape) > len(data.axes):
-                data_arr = data._data.broadcast_to(dshape)
-            else:
-                data_arr = data._data
-            pert = [s_ind[l] for l in daxes]
-            new_data = self._data.transpose(pert) * data_arr
-            return SparseData(daxes, dlabels, new_data)
-        else: 
-            raise ValueError('Incorrect shapes.')
-
-    def replace_axes(self, **axeslabs):
-        """Make a copy of data and replaces axes.
-
-        Parameters
-        ----------
-        axeslabs : dict
-            A dictionary of pairs (new_axis_name, axis_values).
-
-        Returns
-        -------
-        result : SparseData
-            New SparseData object.
-        """
-        new_ax = []
-        new_lab = []
-        for ax, labs in zip(self._axes, self._labels):
-            if ax in axeslabs.keys():
-                nax, nlab = axeslabs[ax]
-                new_ax.append(nax)
-                new_lab.append(nlab)
-            else:
-                new_ax.append(ax)
-                new_lab.append(labs)
-        return SparseData(new_ax, new_lab, self._data)
-
-    @property
-    def axes(self):
-        """Names of axes."""
-        return self._axes
-
-    @property
-    def labels(self):
-        """Axes labels."""
-        return self._labels
-
-    @property
-    def data(self):
-        """Data itself."""
-        return self._data
+        candidates = set(range(len(self._labels)))
+        if c:
+            candidates.intersection_update(self._c_labels.get(c, set()))
+        if i:
+            candidates.intersection_update(self._i_labels.get(i, set()))
+        if j:
+            candidates.intersection_update(self._j_labels.get(j, set()))
+        if k:
+            candidates.intersection_update(self._k_labels.get(k, set()))
+        return list(sorted(candidates))
 
 
-def enumerate_labels(labels):
-    return {label: i for i, label in enumerate(labels)}
-
-def create_enum_rules(labels):
-    enum_rules = []
-    for label_list in labels:
-        rule = enumerate_labels(label_list)
-        enum_rules.append(rule)
-    return enum_rules
-
-
-def create_index(label, rule):
-    return [rule[i][l] for i, l in enumerate(label)]
--- test/test_data.py
+++ test/test_data.py
@@ -1,13 +1,53 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-import sparse
 
 
 from r2s_rfda import data
 
 
-@pytest.mark.parametrize('axes, labels, sdata, answer', [
+@pytest.fixture(scope='module')
+def index():
+    indices = [
+        (2, 1, 0, 3),  # 1
+        (2, 2, 0, 5),  # 2 
+        (4, 0, 0, 4),  # 3
+        (4, 0, 0, 5),  # 4
+        (1, 1, 0, 3),  # 0
+        (5, 1, 0, 3),  # 6
+        (5, 0, 0, 4)   # 5
+    ]
+    return data.SpatialIndex(indices)
+
+
+@pytest.mark.parametrize('labels, answer', [
+    ({'c': 2, 'i': 1, 'j': 0, 'k': 3}, [1]), 
+    ({'c': 2, 'i': 2, 'j': 0, 'k': 5}, [2]),
+    ({'c': 4, 'i': 0, 'j': 0, 'k': 4}, [3]),  
+    ({'c': 4, 'i': 0, 'j': 0, 'k': 5}, [4]), 
+    ({'c': 1, 'i': 1, 'j': 0, 'k': 3}, [0]), 
+    ({'c': 5, 'i': 0, 'j': 0, 'k': 3}, [6]), 
+    ({'c': 5, 'i': 0, 'j': 0, 'k': 4}, [5]),
+    ({'c': 2}, [1, 2]), ({'c': 4}, [3, 4]), ({'c': 5}, [5, 6]), ({'c': 1}, [0]),
+    ({'i': 1}, [0, 1, 6]), ({'i': 1, 'j': 0, 'k': 3}, [0, 1, 6]), 
+    ({'i': 0, 'j': 0, 'k': 4}, [3, 5]), ({'c': 2, 'k': 3}, [1]),
+    ({'c': 6}, []), ({'c': 2, 'i': 1, 'j': 1, 'k': 3}, [])
+])
+def test_index(index, labels, answer):
+    result = index.indices(**labels)
+    assert result == answer
+
+
+@pytest.mark.parametrize('ind, answer', [
+    (1, (2, 1, 0, 3)), (2, (2, 2, 0, 5)), (3, (4, 0, 0, 4)), (4, (4, 0, 0, 5)), 
+    (0, (1, 1, 0, 3)), (6, (5, 1, 0, 3)), (5, (5, 0, 0, 4)), 
+])
+def test_label(index, ind, answer):
+    result = index.label(ind)
+    assert result == answer
+
+
+""" @pytest.mark.parametrize('axes, labels, sdata, answer', [
     (
         ('cell', 'i', 'j'), ((4, 2, 3, 1), (0, 1, 2), (0, 1)),
         {(1, 0, 0): 3, (1, 2, 0): 2, (2, 1, 1): 4, (3, 1, 0): 5, (4, 0, 0): 1},
@@ -117,3 +157,4 @@ def test_replace_axes(tensor, axeslabs, answer):
     for index, value in answer[2].items():
         assert result.data[index] == value
     assert result.data.nnz == len(answer[2])
+ """
